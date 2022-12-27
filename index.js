9cb88545cdc349c32e40e63d17ef1f13b58c0c9c
niphitphon8bit <niphitphon_nt@hotmail.com>
2022-12-27 11:59:50 +0700

    Add connection to database part

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const connection = require('./database');
 
-app.get('/', (req, res) => res.send('Hello world.'));
+app.get('/', (req, res) => res.send('Try: /status, /warehouses, or /warehouses/2'));
+
+app.get('/status', (req, res) => res.send('Success.'));
+
+// app.get('/', (req, res) => res.send('Hello world.'));
+app.get('/account', (req, res) => {
+    connection.query(
+        "SELECT * FROM `wally-money-20`.`wm_account`",
+        (error, results, fields) => {
+            if (error) throw error;
+            res.json(results);
+        }
+    );
+});
+
+app.route('/account/:ac_id')
+    .get((req, res, next) => {
+        connection.query(
+            "SELECT * FROM `wally-money-20`.`wm_account` WHERE ac_id = ?", req.params.ac_id,
+            (error, results, fields) => {
+                if (error) throw error;
+                res.json(results);
+            }
+        );
+    });
 
 app.listen(8080, () => console.log('App is running at: http://localhost:8080'));
